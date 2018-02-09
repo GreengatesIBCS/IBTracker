a6381928d000a6a3fe263b835969ca69f99561d0
7826 <7826@greengates.edu.mx>
2018-02-09 13:18:54 -0600

    adding a file of my own- cf

--- tracker/views.py
+++ tracker/views.py
@@ -1,19 +1,64 @@
-from django.shortcuts import render
 
-from .models import Subject, Topic, SubTopic
+from django.shortcuts import render, redirect
+from .forms import SubjectForm, LoginForm, SupportForm
 
 
+from .models import Subject, Topic, Subtopic, Login, Support
+
+# Create your views here.
 def index(request):
-    """
-    View function for home page of site.
-    """
-    # Generate counts of some of the main objects
+
     num_subjects = Subject.objects.all().count()
     num_topics = Topic.objects.all().count()
+    my_subject = Subject.objects.all()
+    subject_name = my_subject[0].subject_name
+    Login_code = Login.objects.all()
+    Sub_topic = Subtopic.objects.all().count()
 
-    # Render the HTML template index.html with the data in the context variable
     return render(
         request,
         'index.html',
-        context={'num_topics': num_topics, 'num_subjects': num_subjects,},
+        context={'num_topics': num_topics, 'num_subjects': num_subjects, 'subject_name': subject_name,},
     )
+def subject_new(request):
+    if request.method == "POST":
+        #POST, the request
+        form = SubjectForm(request.POST)
+        if form.is_valid():
+            subject = form.save(commit=False)
+            subject.save()
+            return redirect('subject_detail', pk=subject.pk)
+    else:
+        form = SubjectForm()
+    return render(request, 'subject_edit.html', {'form': form})
+
+def Login_new (request):
+    if request.method == "POST":
+        #POST, the request
+        form = LoginForm (request.POST)
+        if form.is_valid():
+            Login = form.save(commit=False)
+            Login.save()
+            return redirect('index')
+
+            return redirect('Login_detail', pk=Login.pk)
+    else:
+        form = LoginForm()
+    return render(request, 'Login_edit.html', {'form': form})
+
+def Support_new (request):
+    if request.method == "POST":
+        # POST, the request
+        form = SupportForm(request.POST)
+        if form.is_valid():
+            Support = form.save(commit=False)
+            Support.save()
+            return redirect('index')
+
+            return redirect('Support_detail', pk=Support.pk)
+    else:
+        form = SupportForm()
+    return render(request, 'Support_edit.html', {'form': form})
+
+
+
